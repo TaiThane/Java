af903073f9d5ca0c3696c3e0590e16dc21662114
pc <pc@Ti>
2024-09-22 00:42:45 +0700

    StudentVer4

---
+++ src/studentver4/IPerson.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentver4;
+
+public interface IPerson {
+    public abstract void addPerson();
+    public abstract void updatePerson(String id);
+    public abstract void displayInfo();
+}
+
